23408c3b4266f2909dec409d6cc609be7eca9eda
Ning <2415731066@qq.com>
2019-04-17 22:22:53 +0800

    first commit

---
+++ src/base/nettest/InetAddressTest.java
@@ -0,0 +1,15 @@
+package base.nettest;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class InetAddressTest {
+    public static void main(String[] args) throws UnknownHostException {
+        InetAddress localHost=InetAddress.getByName("www.sina.com.cn");// InetAddress.getLocalHost();
+        System.out.println(localHost.getHostAddress());
+        System.out.println(localHost.getHostName());
+
+
+
+    }
+}
